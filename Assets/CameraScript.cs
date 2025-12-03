6f8020331d18255c6835a64ff26a12e500da70a1
MattWojtowicz <60191@kobart.be>
2025-12-03 09:54:14 +0100

    3-12-2025

---
+++ Assets/CameraScript.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class PlayerLook : MonoBehaviour
+{
+    public float mouseSensitivity = 1f;
+    public Transform playerCamera;
+
+    private float xRotation = 0f;
+    private float yRotation = 0f;
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+        yRotation += mouseX;
+        playerCamera.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        if (PlayerMovement.aan_het_walrunnen == true)
+        {
+            playerCamera.localRotation = Quaternion.Euler(xRotation, yRotation, 0f);
+
+
+        }
+        else if (PlayerMovement.aan_het_walrunnen == false)
+        {
+            yRotation = 0f;
+            transform.Rotate(Vector3.up * mouseX);
+        }
+    }
+}
